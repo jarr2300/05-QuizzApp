f1ed5d666921b05604fa1ea08f93b89fe2a988f6
Macabuku <jarr2300foros@gmail.com>
2018-12-30 23:00:13 +0100

    Initial Commit

---
+++ 05-QuizzApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  05-QuizzApp
+//
+//  Created by Macabuku on 30/12/18.
+//  Copyright © 2018 Macabuku. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
